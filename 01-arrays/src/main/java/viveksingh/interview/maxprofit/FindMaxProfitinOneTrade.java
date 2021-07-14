5b05f5c7092e4be007aa1979723e96a4de4d4758
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-07-14 08:34:36 +0530

    find max profit in one trade

---
+++ 01-arrays/src/main/java/viveksingh/interview/maxprofit/FindMaxProfitinOneTrade.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package viveksingh.interview.maxprofit;
+
+/**
+ * @author vivek
+ *
+ */
+public class FindMaxProfitinOneTrade {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		final int[] prices_input_1 = { 7, 1, 5, 3, 6, 4 };
+
+		final int[] prices_input_4 = { 7, 1, 5, 7, 6, 4 };
+
+		final int[] prices_input_5 = { 7, 1, 5, 7, 3, 6, 4 };
+
+		final int[] prices_input_2 = { 1, 2, 3, 4, 5 };
+
+		final int[] prices_input_3 = { 7, 6, 4, 3, 1 };
+
+		int maxProfit = maxProfitInOneTrade(prices_input_1);
+		printArray(prices_input_1);
+		System.out.println("Max profit : " + maxProfit + "\n");
+
+		maxProfit = maxProfitInOneTrade(prices_input_4);
+		printArray(prices_input_4);
+		System.out.println("Max profit : " + maxProfit + "\n");
+
+		maxProfit = maxProfitInOneTrade(prices_input_5);
+		printArray(prices_input_5);
+		System.out.println("Max profit : " + maxProfit + "\n");
+
+		maxProfit = maxProfitInOneTrade(prices_input_2);
+		printArray(prices_input_2);
+		System.out.println("Max profit : " + maxProfit + "\n");
+
+		maxProfit = maxProfitInOneTrade(prices_input_3);
+		printArray(prices_input_3);
+		System.out.println("Max profit : " + maxProfit + "\n");
+
+	}
+
+	private static int maxProfitInOneTrade(int[] prices) {
+		int maxProfit = 0;
+		int minTradeValue = prices[0];
+
+		for (int i = 1; i < prices.length; i++) {
+			// Find minimum stock prices in array so far
+			minTradeValue = Math.min(minTradeValue, prices[i]);
+
+			// decide max profit with current day
+			maxProfit = Math.max(maxProfit, (prices[i] - minTradeValue));
+		}
+		
+		System.out.println("Buy price : " + minTradeValue);
+		System.out.println("Sell price : " + (maxProfit + minTradeValue));
+		
+		return maxProfit;
+	}
+
+	/**
+	 * Print input array data.
+	 * 
+	 * @param prices_input_1
+	 */
+	private static void printArray(int[] arr) {
+
+		System.out.print("[");
+		for (int i : arr) {
+			System.out.print(i + ", ");
+		}
+		System.out.println("]");
+	}
+}
