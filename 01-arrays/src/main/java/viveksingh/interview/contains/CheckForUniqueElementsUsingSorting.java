e9a80f89454b89072af7e0f4ff73b8c2f8e0577c
Vivek Singh <16172793+viveksinghrpc@users.noreply.github.com>
2021-07-15 08:39:29 +0530

    Array find duplicates - minor changes

--- 01-arrays/src/main/java/viveksingh/interview/contains/CheckForUniqueElementsUsingSorting.java
+++ 01-arrays/src/main/java/viveksingh/interview/contains/CheckForUniqueElementsUsingSorting.java
@@ -74,9 +74,9 @@ public class CheckForUniqueElementsUsingSorting {
 		ArrayUtils.printArray(arr, "Sorted Array : ");
 
 		// Check for duplicates
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == arr[i + 1]) {
-				System.out.println("Duplicate found : " + arr[i] + ", Index : " + (i + 1));
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] == arr[i - 1]) {
+				System.out.println("Duplicate found : " + arr[i] + ", Index : " + i);
 				return true;
 			}
 		}
