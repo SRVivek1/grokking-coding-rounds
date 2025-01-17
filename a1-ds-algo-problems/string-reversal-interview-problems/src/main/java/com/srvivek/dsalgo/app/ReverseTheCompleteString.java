b216add71bc1a0eaf0434ec4da90358ee4f0624d
srvivek1 <16172793+SRVivek1@users.noreply.github.com>
2025-01-17 14:54:37 +0530

    String reverse the string without reversing the characters of the word

---
+++ a1-ds-algo-problems/string-reversal-interview-problems/src/main/java/com/srvivek/dsalgo/app/ReverseTheCompleteString.java
@@ -0,0 +1,58 @@
+package com.srvivek.dsalgo.app;
+
+/**
+ * Problem:
+ *  Given a string, reverse the order of words in the string without reversing
+ *  the characters of the individual words.
+ *  Example:
+ *      Input : "Hello World from Java";
+ *      Output: "Java from World Hello"
+ *
+ *  Approach:
+ *  -----------
+ *      1. Split the string into words.
+ *      2. Reverse the array of words.
+ *      3. Join the words back into a string.
+ */
+public class ReverseTheCompleteString {
+
+    /**
+     * Reverse the string without reversing the characters of individual words.
+     *  Approach:
+     *  -----------
+     *      1. Split the string into words.
+     *      2. Reverse the array of words.
+     *      3. Join the words back into a string.
+     *
+     * @param str
+     * @return
+     */
+    public static String reverse(String str) {
+
+        // Empty string check
+        if(str == null || str.isEmpty()) {
+            return "";
+        }
+
+        final StringBuilder res = new StringBuilder();
+
+        // Split string with spaces
+        final String [] arr = str.split("\\s+");
+
+        // iterate from last index to first and add in StringBuilder
+        for(int i = arr.length - 1; i >= 0; i--) {
+            res.append(arr[i]);
+            if(i !=0 ) {
+                res.append(" ");
+            }
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        String input = "Hello world from Java";
+        String res = reverse(input);
+        System.out.println("Input: " + input);
+        System.out.println("Output: " + res);
+    }
+}
