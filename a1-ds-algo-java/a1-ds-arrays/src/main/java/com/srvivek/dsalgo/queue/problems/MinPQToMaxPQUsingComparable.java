5fbecdbb0ab90a197940c60db7229571eaedb0e7
srvivek1 <16172793+SRVivek1@users.noreply.github.com>
2025-01-28 06:01:54 +0530

    Max PQ using java Min PriorityQueue implementation

--- a1-ds-algo-java/a1-ds-arrays/src/main/java/com/srvivek/dsalgo/queue/problems/MinPQToMaxPQUsingComparable.java
+++ a1-ds-algo-java/a1-ds-arrays/src/main/java/com/srvivek/dsalgo/queue/problems/MinPQToMaxPQUsingComparable.java
@@ -1,7 +1,6 @@
 package com.srvivek.dsalgo.queue.problems;
 
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -11,36 +10,67 @@ public class MinPQToMaxPQUsingComparable {
 
         final int[] data = new int[]{3, 10, 1, 15, 100, 22};
 
-        final Queue<Integer> defaultPQ = new PriorityQueue<>();
+        Queue<Integer> maxPQ = constructMaxPQueue(data);
+        print(maxPQ);
+    }
 
+    /**
+     * Create Max priority queue using provided data.
+     *
+     * @param data
+     * @return
+     */
+    private static Queue<Integer> constructMaxPQueue(int[] data) {
         /*
          * Comparator returns a negative integer, zero, or a positive integer as the first argument
          * is less than, equal to, or greater than the second.
          */
         final Queue<Integer> maxPQ = new PriorityQueue<>(new Comparator<Integer>() {
+
+            /**
+             * {@inheritDoc}
+             *
+             * Reverse the return value to get Max Priority Queue.
+             *
+             * arg1 < arg2 ----> 1 [Default / MinPQ will return 1]
+             * arg1 < arg2 ----> 0 [Default / MinPQ will return 0]
+             * arg1 < arg2 ----> -1 [Default / MinPQ will return -1]
+             *
+             * @param arg1 the first object to be compared.
+             * @param arg2 the second object to be compared.
+             * @return
+             */
             @Override
             public int compare(Integer arg1, Integer arg2) {
-                return arg1 <= arg2 ? 0 : -1;
+                if (arg1 > arg2) {
+                    return -1;
+                } else if (arg1.equals(arg2)) {
+                    return 0;
+                } else {
+                    return 1;
+                }
             }
         });
 
-        for (int i = 0; i < data.length; i++) {
-            int t = data[i];
-            defaultPQ.offer(t);
+        StringBuilder sb = new StringBuilder("Input array: [");
+        for (int t : data) {
             maxPQ.offer(t);
+            sb.append(t).append(", ");
         }
-
-        System.out.println("=============== toString() ========================");
-        System.out.println("Default Queue: " + defaultPQ.toString());
-        System.out.println("MaxPQ : " + maxPQ.toString());
-
-        System.out.println("=============== poll() ========================");
-        print("Default Queue: ", defaultPQ);
-        print("MaxPQ : ", maxPQ);
+        sb.append("]");
+        System.out.println(sb);
+        return maxPQ;
     }
 
-    private static void print(String message, Queue<Integer> queue) {
-        StringBuilder sb = new StringBuilder(message).append(" {");
+    /**
+     * Print Queue data using `poll()` API.
+     *
+     * @param queue
+     */
+    private static void print(Queue<Integer> queue) {
+
+        queue = new PriorityQueue<>(queue);
+        StringBuilder sb = new StringBuilder("MaxPQ : ").append(" {");
 
         while (!queue.isEmpty()) {
             sb.append(queue.poll()).append(", ");
@@ -49,3 +79,12 @@ public class MinPQToMaxPQUsingComparable {
         System.out.println(sb.toString());
     }
 }
+
+/**
+ * Output
+ * ====================
+ * Input array: [3, 10, 1, 15, 100, 22, ]
+ * MaxPQ :  {100, 22, 15, 10, 3, 1, }
+ *
+ *
+ */
