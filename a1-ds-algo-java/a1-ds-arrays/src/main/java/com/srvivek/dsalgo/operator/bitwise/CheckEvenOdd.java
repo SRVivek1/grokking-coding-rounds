fc7cb835447bc2844c2dc440c5d2eb682179564b
srvivek1 <16172793+SRVivek1@users.noreply.github.com>
2025-02-11 07:02:16 +0530

    bitwise operator to check odd / even

---
+++ a1-ds-algo-java/a1-ds-arrays/src/main/java/com/srvivek/dsalgo/operator/bitwise/CheckEvenOdd.java
@@ -0,0 +1,35 @@
+package com.srvivek.dsalgo.operator.bitwise;
+
+public class CheckEvenOdd {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        System.out.printf("%d is even : %b.%n", x, isEven(x));
+
+        int[] inputs = new int[25];
+        for (int i = 1; i <= inputs.length; i++) inputs[i-1] = i;
+
+        checkEven(inputs);
+    }
+
+    /**
+     * @param num input number
+     * @return true for even number
+     */
+    public static boolean isEven(int num) {
+        return (num & 1) == 0;
+    }
+
+    /**
+     * check numbers if they are odd / even.
+     *
+     * @param nums int array
+     */
+    public static void checkEven(int[] nums) {
+
+        for (int i : nums) {
+            System.out.printf("%d is even : %b.%n", i, isEven(i));
+        }
+    }
+}
